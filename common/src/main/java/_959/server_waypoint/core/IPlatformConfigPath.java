17bd2e7a04197649f167b33c4253e73e99bbf056
Garze23 <149205373+Garze23@users.noreply.github.com>
2026-04-08 18:11:57 +0200

    test

--- common/src/main/java/_959/server_waypoint/core/IPlatformConfigPath.java
+++
@@ -1,7 +0,0 @@
-package _959.server_waypoint.core;
-
-import java.nio.file.Path;
-
-public interface IPlatformConfigPath {
-    Path getAssignedConfigDirectory();
-}
--- common/src/main/java/_959/server_waypoint/util/TriConsumer.java
+++
@@ -1,6 +0,0 @@
-package _959.server_waypoint.util;
-
-@FunctionalInterface
-public interface TriConsumer<T, U, V> {
-    void accept(T k, U v, V s);
-}
